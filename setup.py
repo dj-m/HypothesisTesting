2ff76b92a244dfb69708540926ef6f0fb7d31b09
dj-m <domingomoronta@gmail.com>
2020-08-07 10:27:57 -0700

    Added Data Science Cookiecutter folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='aAnalyzing whether there's a significant difference between Apple's App Store and Google's Play Store in regards to app ratings.',
+    author='Domingo Moronta',
+    license='',
+)
